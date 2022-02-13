044eecdcba41ae72b5b0f890b30e153a5a0515a4
game-pixel <divancoder10@gmail.com>
2022-02-13 03:11:59 +0000

    commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,60 +1,7 @@
 <script>
     import supabase from '$lib/db';
 
-    async function logout() {
-   	 const { error } = await supabase.auth.signOut();
-
-   	 if (error) alert(error.message); // alert if error
-    }
-
-</script>
-
-<br>
-<br>
-<h1>Dashboard</h1>
-<table class="table table-sm table-dark table-bordered table caption-top">
-    <caption>School Timetable</caption>
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">1</th>
-        <th scope="col">2</th>
-        <th scope="col">3</th>
-        <th scope="col">4</th>
-        <th scope="col">-</th>
-        <th scope="col">5</th>
-        <th scope="col">6</th>
-        <th scope="col">7</th>
-        <th scope="col">8</th>
-        <th scope="col">9</th>
-        <th scope="col">10</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">Mon</th>
-        
-      </tr>
-      <tr>
-        <th scope="row">Tue</th>
-        
-      </tr>
-      <tr>
-        <th scope="row">Wed</th>
-   
-      </tr>
-      <tr>
-        <th scope="row">Thu</th>
-       
-      </tr>
-      <tr>
-        <th scope="row">Fri</th>
-
-      </tr>
-    </tbody>
-  </table>
-
-  let timetable = {
+    let timetable = {
 	Monday: [
   	{
     	name: "PH",
@@ -227,6 +174,63 @@
 	],
   };
 
+
+    async function logout() {
+   	 const { error } = await supabase.auth.signOut();
+
+   	 if (error) alert(error.message); // alert if error
+    }
+
+</script>
+
+
+
+<br>
+<br>
+<h1>Dashboard</h1>
+<table class="table table-sm table-dark table-bordered table caption-top">
+    <caption>School Timetable</caption>
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">1</th>
+        <th scope="col">2</th>
+        <th scope="col">3</th>
+        <th scope="col">4</th>
+        <th scope="col">-</th>
+        <th scope="col">5</th>
+        <th scope="col">6</th>
+        <th scope="col">7</th>
+        <th scope="col">8</th>
+        <th scope="col">9</th>
+        <th scope="col">10</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">Mon</th>
+        
+      </tr>
+      <tr>
+        <th scope="row">Tue</th>
+        
+      </tr>
+      <tr>
+        <th scope="row">Wed</th>
+   
+      </tr>
+      <tr>
+        <th scope="row">Thu</th>
+       
+      </tr>
+      <tr>
+        <th scope="row">Fri</th>
+
+      </tr>
+    </tbody>
+  </table>
+
+  
 
   <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
