cb3c05a7f5ac9517cf9c799e46534eeb4f96f76e
game-pixel <divancoder10@gmail.com>
2022-02-13 02:38:31 +0000

    commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,9 +17,17 @@
     <thead>
       <tr>
         <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">1</th>
+        <th scope="col">2</th>
+        <th scope="col">3</th>
+        <th scope="col">4</th>
+        <th scope="col">-</th>
+        <th scope="col">5</th>
+        <th scope="col">6</th>
+        <th scope="col">7</th>
+        <th scope="col">8</th>
+        <th scope="col">9</th>
+        <th scope="col">10</th>
       </tr>
     </thead>
     <tbody>
