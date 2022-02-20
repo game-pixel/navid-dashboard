7d06711cf5094eae36405c0ef9d6e18f4078f536
game-pixel <divancoder10@gmail.com>
2022-02-20 02:57:32 +0000

    commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -262,8 +262,8 @@
 	<div class="modal-dialog">
 	  <div class="modal-content">
 		<div class="modal-header">
-		  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cancel"></button>
+		  <h5 class="modal-title" id="exampleModalLabel">Edit Time Slot</h5>
+		  <button type="button" class="btn-cancel" data-bs-dismiss="modal" aria-label="Cancel"></button>
 		</div>
 		<div class="modal-body">
 		  
